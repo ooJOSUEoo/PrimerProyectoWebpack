bda69339aea21060a7e38db1d95e57f7b9d0338d
Josue <sjosue5082002@gmail.com>
2022-02-03 08:44:10 -0600

    Importando estilos de forma dinámica

--- src/js/funciones.js
+++ src/js/funciones.js
@@ -1,3 +1,5 @@
+import '../css/funciones.css';
+
 export const saludar = (nombre) => {
     const h1 = document.createElement('h1');
     h1.innerText = `Hola ${nombre}`;
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports ={
                 options: {
                     sources: false,
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ],
             }
         ]
     },
